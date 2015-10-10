73c8514f2bf3714c36f254ee5959f39dcc5465d5
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2015-10-11 03:11:10 +0900

    add singleton class

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Framework
+{
+	/// <summary>
+	/// シングルトン化を提供する
+	/// </summary>
+	public abstract class Singleton<T> where T : Singleton<T> , new ()
+	{
+		private static T _instance;
+
+		/// <summary>
+		/// シングルトンのインスタンス
+		/// </summary>
+		/// <value>The instance.</value>
+		public static T instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new T ();
+				}
+				return _instance;
+			}
+		}
+
+		/// <summary>
+		/// シングルトンのインスタンス取得
+		/// </summary>
+		/// <returns>The instance.</returns>
+		public static T GetInstance ()
+		{
+			return instance;
+		}
+
+		protected Singleton () {}
+	}
+}
---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// GameObjectのシングルトン化を提供する
+/// </summary>
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+{
+	private static T _instance;
+
+	/// <summary>
+	/// シングルトンのインスタンス
+	/// </summary>
+	/// <value>The instance.</value>
+	public static T Instance {
+		get {
+			if (_instance == null)
+            {
+				_instance = FindObjectOfType<T> ();
+				if (_instance == null)
+                {
+					GameObject go = new GameObject (typeof (T).Name, typeof (T));
+					_instance = go.GetComponent<T> ();
+                    _instance.CheckInstance ();
+				}
+			}
+			return _instance;
+		}
+	}
+
+	/// <summary>
+	/// シングルトンのインスタンス取得
+	/// </summary>
+	/// <returns>The instance.</returns>
+	public static T GetInstance ()
+	{
+		return Instance;
+	}
+
+    [SerializeField]
+    private bool _isDontDestroyOnLoad = true;
+
+    private bool isExecuteAwake = false;
+    private bool isExecuteStart = false;
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	protected SingletonMonoBehaviour () {}
+
+    private void Awake ()
+    {
+        if (isExecuteAwake == true)
+            return;
+        if (CheckInstance () == false)
+            return;
+
+        isExecuteAwake = true;
+        OnAwake ();
+    }
+
+    private void Start ()
+    {
+        if (isExecuteStart == true)
+            return;
+
+        isExecuteStart = true;
+        OnStart ();
+    }
+
+    private void OnDestroy ()
+    {
+        if (_isDontDestroyOnLoad == true)
+            return;
+        if (this != _instance)
+            return;
+        
+        OnFinalize ();
+        _instance = null;
+    }
+
+	protected virtual void OnInitialize ()
+	{
+
+	}
+
+    protected virtual void OnFinalize ()
+    {
+
+    }
+
+    protected virtual void OnAwake ()
+    {
+
+    }
+
+	protected virtual void OnStart ()
+	{
+
+	}
+
+	protected virtual void Update ()
+	{
+
+	}
+
+	/// <summary>
+	/// インスタンスが存在するか確認する
+	/// </summary>
+	/// <returns><c>true</c>, if instance was checked, <c>false</c> otherwise.</returns>
+	protected bool CheckInstance()
+	{
+		if (_instance == null)
+        {
+            _instance = (T)this;
+            _instance.OnInitialize ();
+            if (_instance._isDontDestroyOnLoad == true)
+    			DontDestroyOnLoad (this.gameObject);
+			return true;
+		}
+        else if (_instance == this)
+        {
+			return true;
+		}
+
+		Destroy (this);
+		return false;
+	}
+}
