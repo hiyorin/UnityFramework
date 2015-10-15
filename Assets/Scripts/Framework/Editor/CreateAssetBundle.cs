20bc2ac4be18abb38bb2d252e3c339882ab88404
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2015-10-16 02:47:39 +0900

    簡易アセットバンドル生成

---
+++ Assets/Scripts/Framework/Editor/CreateAssetBundle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem ("Tools/Resource/Build AssetBundles (Standalone)")]
+    private static void BuildAllAssetBundlesStandalone ()
+    {
+        string outputPath = ResourceManagerSettings.StancaloneAssetBundleOutputPath;
+        BuildAllAssetBundles (outputPath);
+    }
+
+    [MenuItem ("Tools/Resource/Build AssetBundles (Production)")]
+    private static void BuildAllAssetBundlesProduction ()
+    {
+        string outputPath = ResourceManagerSettings.ProductionAssetBundleOutputPath;
+        BuildAllAssetBundles (outputPath);
+    }
+
+    private static void BuildAllAssetBundles (string outputPath)
+    {
+        string targetFolderName = EditorUserBuildSettings.activeBuildTarget.ToString ();
+
+        string properPath = Path.Combine (outputPath, targetFolderName);
+        string[] splitPath = properPath.Split (Path.PathSeparator);
+        string path = string.Empty;
+        foreach (var row in splitPath)
+        {
+            path = Path.Combine (path, row);
+            if (Directory.Exists (path) == false)
+                Directory.CreateDirectory (path);
+        }
+
+        BuildPipeline.BuildAssetBundles (
+            properPath,
+            BuildAssetBundleOptions.None,
+            EditorUserBuildSettings.activeBuildTarget
+        );
+    }
+}
---
+++ Assets/Scripts/Framework/Resource/ResourceManagerSettings.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+
+[InitializeOnLoad]
+#endif
+public class ResourceManagerSettings : ScriptableObject
+{
+    private const string SettingsAssetName = "ResourceManagerSettings";
+    private const string SettingsAssetPath = "Resources";
+    private const string SettingsAssetExtension = ".asset";
+
+    private static ResourceManagerSettings instance;
+
+    private static ResourceManagerSettings Instance ()
+    {
+        if (instance == null)
+        {
+            instance = Resources.Load (SettingsAssetName) as ResourceManagerSettings;
+            if (instance == null)
+            {
+                instance = CreateInstance<ResourceManagerSettings> ();
+#if UNITY_EDITOR
+                string properPath = Path.Combine (Application.dataPath, SettingsAssetPath);
+                if (Directory.Exists (properPath) == false)
+                    AssetDatabase.CreateFolder ("Assets", SettingsAssetPath);
+
+                string fullPath = Path.Combine (Path.Combine("Assets", SettingsAssetPath),
+                    SettingsAssetName + SettingsAssetExtension
+                );
+                AssetDatabase.CreateAsset (instance, fullPath);
+#endif
+            }
+        }
+        return instance;
+    }
+
+#if UNITY_EDITOR
+    [MenuItem ("Tools/Resource/Settings")]
+    public static void Edit ()
+    {
+        Selection.activeObject = Instance ();
+    }
+
+    private static void DirtyEditor ()
+    {
+        EditorUtility.SetDirty(Instance ());
+    }
+#endif
+
+    [SerializeField, Header ("AssetBundle")]
+    private string _standaloneAssetBundleOutputPath = "Assets/StreamingAssets/";
+    public static string StancaloneAssetBundleOutputPath { get { return Instance ()._standaloneAssetBundleOutputPath; } }
+
+    [SerializeField]
+    private string _productionAssetBundleOutoutPath = "AssetBundles/";
+    public static string ProductionAssetBundleOutputPath { get { return Instance ()._productionAssetBundleOutoutPath; } }
+}
