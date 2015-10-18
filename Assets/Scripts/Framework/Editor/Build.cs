4e3ef65b9a5a6f23cb7fb2979bb586d220d256ad
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2015-10-18 10:52:17 +0900

    バイナリのビルドスクリプト追加

--- Assets/Scripts/Framework/Editor/Build.cs
+++ Assets/Scripts/Framework/Editor/Build.cs
@@ -1,17 +1,77 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
 
 public class Build
 {
-    [MenuItem ("Tools/Build/AssetBundles (Standalone)")]
-    private static void BuildAllAssetBundlesStandalone ()
+    private static readonly Dictionary<BuildTarget, string> Extension = new Dictionary<BuildTarget, string> {
+        { BuildTarget.Android,   "apk" },
+        { BuildTarget.iOS,       "ipa" },
+    };
+
+    [MenuItem ("Tools/Build/Build Player (Develop)")]
+    private static void BuildPlayerDevelop ()
+    {
+        string extension = string.Empty;
+        Extension.TryGetValue (EditorUserBuildSettings.activeBuildTarget, out extension);
+        string locationPathName = EditorUtility.SaveFilePanel (
+            "build develop", "", PlayerSettings.productName, extension);
+        if (string.IsNullOrEmpty (locationPathName) == true)
+            return;
+        BuildAllAssetBundlesDevelop ();
+        BuildPlayer (locationPathName, EditorUserBuildSettings.activeBuildTarget,
+            BuildOptions.Development);
+    }
+
+    [MenuItem ("Tools/Build/Build and Run Player (Develop)")]
+    private static void BuildAndRunPlayerDevelop ()
+    {
+        string extension = string.Empty;
+        Extension.TryGetValue (EditorUserBuildSettings.activeBuildTarget, out extension);
+        string locationPathName = EditorUtility.SaveFilePanel (
+            "build & run develop", "", PlayerSettings.productName, extension);
+        if (string.IsNullOrEmpty (locationPathName) == true)
+            return;
+        BuildAllAssetBundlesProduction ();
+        BuildPlayer (locationPathName, EditorUserBuildSettings.activeBuildTarget,
+            BuildOptions.Development | BuildOptions.AutoRunPlayer);
+    }
+
+    [MenuItem ("Tools/Build/Build Player (Production)")]
+    private static void BuildPlayerProduction ()
+    {
+        string extension = string.Empty;
+        Extension.TryGetValue (EditorUserBuildSettings.activeBuildTarget, out extension);
+        string locationPathName = EditorUtility.SaveFilePanel (
+            "build production", "", PlayerSettings.productName, extension);
+        if (string.IsNullOrEmpty (locationPathName) == true)
+            return;
+        BuildPlayer (locationPathName, EditorUserBuildSettings.activeBuildTarget,
+            BuildOptions.None);
+    }
+
+    [MenuItem ("Tools/Build/Build and Run Player (Production)")]
+    private static void BuildAndRunPlayerProduction ()
+    {
+        string extension = string.Empty;
+        Extension.TryGetValue (EditorUserBuildSettings.activeBuildTarget, out extension);
+        string locationPathName = EditorUtility.SaveFilePanel (
+            "build & run production", "", PlayerSettings.productName, extension);
+        if (string.IsNullOrEmpty (locationPathName) == true)
+            return;
+        BuildPlayer (locationPathName, EditorUserBuildSettings.activeBuildTarget,
+            BuildOptions.None | BuildOptions.AutoRunPlayer);
+    }
+
+    [MenuItem ("Tools/Build/Build AssetBundles (Develop)")]
+    private static void BuildAllAssetBundlesDevelop ()
     {
         string outputPath = ResourceManagerSettings.StancaloneAssetBundleOutputPath;
         BuildAllAssetBundles (outputPath);
     }
 
-    [MenuItem ("Tools/Build/AssetBundles (Production)")]
+    [MenuItem ("Tools/Build/Build AssetBundles (Production)")]
     private static void BuildAllAssetBundlesProduction ()
     {
         string outputPath = ResourceManagerSettings.ProductionAssetBundleOutputPath;
@@ -26,10 +86,33 @@ public class Build
             Debug.LogFormat ("{0} : {1}", i.ToString ("000"), assetBundleNames [i]);
     }
 
+    private static void BuildAndroid ()
+    {
+        BuildPlayer (PlayerSettings.productName + "." + Extension [BuildTarget.Android], BuildTarget.Android, BuildOptions.None);
+    }
+
+    private static void BuildIOS ()
+    {
+        BuildPlayer (PlayerSettings.productName + "." + Extension [BuildTarget.iOS], BuildTarget.iOS, BuildOptions.None);
+    }
+
+    private static void BuildPlayer (string locationPathName, BuildTarget buildTarget, BuildOptions buildOptions)
+    {
+        List<string> listSceneName = new List<string> ();
+        foreach (var scene in SceneManagerSettings.ListBuidlInScene)
+        {
+            if (scene == null)
+                continue;
+            listSceneName.Add (AssetDatabase.GetAssetPath (scene));
+        }
+        BuildPipeline.BuildPlayer (listSceneName.ToArray (), locationPathName,
+            buildTarget, buildOptions | BuildOptions.Il2CPP);
+        Debug.Log ("Build Player Complete");
+    }
+
     private static void BuildAllAssetBundles (string outputPath)
     {
         string targetFolderName = EditorUserBuildSettings.activeBuildTarget.ToString ();
-
         string properPath = Path.Combine (outputPath, targetFolderName);
         string[] splitPath = properPath.Split (Path.PathSeparator);
         string path = string.Empty;
@@ -39,12 +122,12 @@ public class Build
             if (Directory.Exists (path) == false)
                 Directory.CreateDirectory (path);
         }
-
         BuildPipeline.BuildAssetBundles (
             properPath,
             BuildAssetBundleOptions.None,
             EditorUserBuildSettings.activeBuildTarget
         );
+        Debug.Log ("Build AssetBundle Complete");
     }
 
 //    [MenuItem ("Tools/Test")]
