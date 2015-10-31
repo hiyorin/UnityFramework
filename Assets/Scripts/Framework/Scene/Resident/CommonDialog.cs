e1d4ef33ae4f0c8bf3776804e316870ecd250781
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2015-11-01 04:16:13 +0900

    CommonDialog追加

---
+++ Assets/Scripts/Framework/Scene/Resident/CommonDialog.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Framework.UI;
+
+namespace Framework.Scene.Resident
+{
+    public class CommonDialog : UIMonoBehaviour
+    {
+        [SerializeField]
+        private CommonDialogController _announce = null;
+        [SerializeField]
+        private CommonDialogController _oneButton = null;
+        [SerializeField]
+        private CommonDialogController _twoButton = null;
+        [SerializeField]
+        private CommonDialogController _treeButton = null;
+
+        public CommonDialogController announce {
+            get { return _announce; }
+        }
+
+        public CommonDialogController oneButton {
+            get { return _oneButton; }
+        }
+
+        public CommonDialogController twoButton {
+            get { return _twoButton; }
+        }
+
+        public CommonDialogController treeButton {
+            get { return _treeButton; }
+        }
+
+        public void Initialize ()
+        {
+            RemoveListener (_announce);
+            RemoveListener (_oneButton);
+            RemoveListener (_twoButton);
+            RemoveListener (_treeButton);
+        }
+
+        public bool ContainsController (CommonDialogController controller)
+        {
+            if (announce.Equals (controller) == true)
+                return true;
+            if (oneButton.Equals (controller) == true)
+                return true;
+            if (twoButton.Equals (controller) == true)
+                return true;
+            if (treeButton.Equals (controller) == true)
+                return true;
+            return false;
+        }
+
+        private void RemoveListener (CommonDialogController controller)
+        {
+            if (controller == null)
+                return;
+
+            controller.onOpen.RemoveAllListeners ();
+            controller.onClose.RemoveAllListeners ();
+            controller.onClickOneButton.RemoveAllListeners ();
+            controller.onClickTwoButton.RemoveAllListeners ();
+            controller.onClickTreeButton.RemoveAllListeners ();
+
+            controller.gameObject.SetActiveSafe (false);
+        }
+    }
+}
---
+++ Assets/Scripts/Framework/Scene/Resident/CommonDialogController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Framework.UI;
+using DG.Tweening;
+
+namespace Framework.Scene.Resident
+{
+    [RequireComponent (typeof (UICanvasPanel))]
+    public class CommonDialogController : MonoBehaviour
+    {
+        public class DialogEvent : UnityEvent<CommonDialogController> {}
+
+        public readonly DialogEvent onOpen = new DialogEvent ();
+        public readonly DialogEvent onClose = new DialogEvent ();
+        public readonly DialogEvent onClickOneButton = new DialogEvent ();
+        public readonly DialogEvent onClickTwoButton = new DialogEvent ();
+        public readonly DialogEvent onClickTreeButton = new DialogEvent ();
+
+        [SerializeField]
+        private float _fadeDuration = 0.25f;
+        [SerializeField]
+        private Text _message = null;
+        [SerializeField]
+        private Button _oneButton = null;
+        [SerializeField]
+        private Text _oneButtonText = null;
+        [SerializeField]
+        private Button _twoButton = null;
+        [SerializeField]
+        private Text _twoButtonText = null;
+        [SerializeField]
+        private Button _treeButton = null;
+        [SerializeField]
+        private Text _treeButtonText = null;
+
+        protected UICanvasPanel _panel = null;
+
+        private void Awake ()
+        {
+            _panel = GetComponent<UICanvasPanel> ();
+        }
+
+        private void Start ()
+        {
+            if (_oneButton != null)
+                _oneButton.onClick.AddListener (OnClickOneButton);
+
+            if (_twoButton != null)
+                _twoButton.onClick.AddListener (OnClickTwoButton);
+
+            if (_treeButton != null)
+                _treeButton.onClick.AddListener (OnClickTwoButton);
+        }
+
+        public void Open ()
+        {
+            gameObject.SetActiveSafe (true);
+            _panel.FadeIn (_fadeDuration, OnOpenComplete);
+        }
+
+        public void Close ()
+        {
+            _panel.FadeOut (_fadeDuration, OnCloseComplete);
+        }
+
+        public void SetText (string message,
+            string oneButtonText=null, string twoButtonText=null, string treeButtonText=null)
+        {
+            _message.text = message;
+            if (_oneButtonText != null)
+                _oneButtonText.text = oneButtonText;
+            if (_twoButtonText != null)
+                _twoButtonText.text = twoButtonText;
+            if (_treeButtonText != null)
+                _treeButtonText.text = treeButtonText;
+        }
+
+        private void OnOpenComplete ()
+        {
+            onOpen.Invoke (this);
+        }
+
+        private void OnCloseComplete ()
+        {
+            gameObject.SetActiveSafe (false);
+            onClose.Invoke (this);
+        }
+
+        private void OnClickOneButton ()
+        {
+            onClickOneButton.Invoke (this);
+            Close ();
+        }
+
+        private void OnClickTwoButton ()
+        {
+            onClickTwoButton.Invoke (this);
+            Close ();
+        }
+
+        private void OnClickTreeButton ()
+        {
+            onClickTreeButton.Invoke (this);
+            Close ();
+        }
+    }
+}
