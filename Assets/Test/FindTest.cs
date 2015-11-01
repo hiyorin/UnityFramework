35341973327751a95d0268de8dbaf6f435551210
kuwabara kazuma <ponkotsu.hiyorin@gmail.com>
2015-11-01 20:14:59 +0900

    GetComponent、Findテスト

---
+++ Assets/Test/FindTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FindTest : MonoBehaviour
+{
+    void Start ()
+    {
+        Debug.Log ("Find Child_1 " + transform.Find ("Child_1"));
+        Debug.Log ("Find Child_2 " + transform.Find ("Child_2"));
+        Debug.Log ("Find Child_3 " + transform.Find ("Child_3"));
+
+        Debug.Log ("FindChild Child_1 " + transform.FindChild ("Child_1"));
+        Debug.Log ("FindChild Child_2 " + transform.FindChild ("Child_2"));
+        Debug.Log ("FindChild Child_3 " + transform.FindChild ("Child_3"));
+    }
+}
---
+++ Assets/Test/GetComponentTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GetComponentTest : MonoBehaviour
+{
+    void Start ()
+    {
+        Debug.Log (GetComponent<MeshCollider> ());
+        Debug.Log (GetComponentInChildren<BoxCollider> ());
+        Debug.Log (GetComponentInChildren<SphereCollider> ());
+        Debug.Log (GetComponentInChildren<CapsuleCollider> ());
+    }
+}
